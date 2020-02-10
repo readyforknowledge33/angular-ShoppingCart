098760eabc2a8b8aa6d5693d8bc2af8684288181
readyforknowledge33 <readyforknowledge33@gmail.com>
2020-02-10 14:26:11 +0800

    2 files modified

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -7,8 +7,7 @@ import {Input} from '@angular/core';
   styleUrls: ['./product-alerts.component.css']
 })
 export class ProductAlertsComponent implements OnInit {
-
-  @Input() product;
+@Input() product;
   constructor() { }
 
   ngOnInit() {
